162fcd089eba1715ef550dc37e0c805eacd82bd3
Edson Carlos de Melo Ferreira <edsoncarlosdemeloferreira@gmail.com>
2024-02-14 19:04:17 -0300

    Trabalhando com varios layouts

---
+++ LayoutsMultiplos/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LayoutsMultiplos
+{
+    public class Tema
+    {
+        public static string TemaAtual()
+        {
+            var url = HttpContext.Current.Request.Url.Host;
+            url = url.Replace("www.", "");
+            return url;
+        }
+
+    }
---
+++ LayoutsMultiplos/Helpers/Tema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace LayoutsMultiplos
+{
+    public class Tema
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public Tema(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string TemaAtual()
+        {
+            var url = _httpContextAccessor.HttpContext.Request.Host.Value;
+            url = url.Replace("www.", "");
+            return url;
+        }
+    }
+}
